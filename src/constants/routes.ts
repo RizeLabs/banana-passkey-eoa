239f6447d4bc4efa78d85b5104215d1d41db4f6b
Rishabh Gupta <rishotics@gmail.com>
2023-06-13 00:27:05 +0530

    removed .env

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -3,4 +3,4 @@ export const VERIFICATION_LAMBDA_URL = 'https://muw05wa93c.execute-api.us-east-1
 export const BANANA_SERVER = 'https://banana-server.xyz'
 export const GET_WALLETCRED_ROUTE = '/get-user-credentials'
 export const IS_WALLETNAME_UNIQUE_ROUTE = '/check-walletname-exists'
-export const JSON_RPC_PROVIDER = 'https://polygon-mumbai.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}'
+export const JSON_RPC_PROVIDER = 'https://polygon-mumbai.g.alchemy.com/v2/cNkdRWeB8oylSQJSA2V3Xev2PYh5YGr4'
